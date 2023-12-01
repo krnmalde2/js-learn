fdadb5f4ef89986cc5c115f354ede60e2109fbd2
krnmalde2 <krnmalde2@gmail.com>
2023-12-01 05:20:36 +0000

    datatypes

---
+++ 01/datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; // these line tell compiler to use new version of javascript
+
+/*primitive datatypes
+String,number,boolean,symbol,bigint,undefined,null
+*/
+/*
+Refrence type or non primitive type
+object,array,function
+*/
+
+//alert(9+3) alert will not work in node it will work on browser console
+
+console.log(3
+    + 3); // code readibility should be high
+
+
+let name = "Raj"
+let age = 45
+let isLoggedIn = true
+let state;
+let temprature = null
+
+/*
+number range = 2 power of 53
+bigint
+string = ""
+boolean = true/false
+null = standlone value not 0 it means empty
+undefined = 
+symbol = unique
+*/
+
+//object
+
+console.log(typeof(state))
+console.log(typeof temprature )
+
