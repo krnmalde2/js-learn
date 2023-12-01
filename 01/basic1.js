189dc7923e55b4ae2fdd599656675777e8a5103c
krnmalde2 <krnmalde2@gmail.com>
2023-12-01 05:03:51 +0000

    variable basics

---
+++ 01/basic1.js
@@ -0,0 +1,10 @@
+let name = "Raj";
+const age = 45 
+const cityname = "rahul";
+console.log(name);
+console.log(age);
+// const = 54
+// var state = "mumbai"; prefer not to use var beacause of issue in funtional scope and global scope
+passcode = 1523;
+
+console.table([name,age,passcode,cityname])
