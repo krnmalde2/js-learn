c1d8f43cef65ed275a50c8c7f963715f5e77b927
krnmalde2 <krnmalde2@gmail.com>
2023-12-01 07:23:05 +0000

    added readon for null

--- 01/comparison.js
+++ 01/comparison.js
@@ -12,6 +12,17 @@ console.log(null > 0);
 console.log(null <= 0);
 console.log(null < 0); //to study
 console.log(null == 0);
+console.log(null == null);
+
+// Mathematically, that’s strange. The last result states that "null is greater than or equal to zero", so in one of the comparisons above it must be true, but they are both false.
+
+// The reason is that an equality check == and comparisons > < >= <= work differently. Comparisons convert null to a number, treating it as 0. That’s why (3) null >= 0 is true and (1) null > 0 is false.
+
+// On the other hand, the equality check == for undefined and null is defined such that, without any conversions, they equal each other and don’t equal anything else. That’s why (2) null == 0 is false.
+
+
+
+
 console.log(undefined > 0);
 console.log(undefined == 0);
 
